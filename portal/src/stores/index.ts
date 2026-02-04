d046364a51f647bb954e73abc611019190303491
Jonathan <pavilion.vps@gmail.com>
2026-02-04 13:07:02 +1100

    feat(portal): add shell access and log streaming
    Signed-off-by: Jonathan <pavilion.vps@gmail.com>

--- portal/src/stores/index.ts
+++ portal/src/stores/index.ts
@@ -1,3 +1,4 @@
 export { useWalletStore, type WalletState, type WalletType } from './walletStore';
 export { useOrderStore, selectFilteredOrders, type Order, type OrderStatus } from './orderStore';
 export { useUIStore, selectUnreadNotificationCount, type Theme, type Toast, type Notification } from './uiStore';
+export { useIdentityStore, type IdentityState } from './identityStore';
